30b35a506a936a250516ec3b77327fe0849c6581
codexpedia <codexpedia.com@gmail.com>
2018-02-20 22:28:13 -0600

    initial commit

---
+++ app/src/main/java/com/codexpedia/recyclerview/Item.java
@@ -0,0 +1,18 @@
+package com.codexpedia.recyclerview;
+
+// 12/30/15
+public class Item {
+
+    private String name;
+
+    public Item(String n) {
+        name = n;
+    }
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
